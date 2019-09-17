ea2555df05f3fd3f29ebff2b8fbc10ae7043e66e
Arthur TRAN-TAO <zatoster.mc@gmail.com>
2019-09-17 10:55:21 +0200

    Initial commit

---
+++ src/main/java/fr/rphstudio/codingdojo/students/Student70.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.rphstudio.codingdojo.students;
+
+import fr.rphstudio.codingdojo.game.Pod;
+import fr.rphstudio.codingdojo.game.PodPlugIn;
+
+/**
+ *
+ * @author Romuald GRIGNON
+ */
+public class Student70 extends PodPlugIn {
+    public Student70(Pod p){
+        super(p);
+    }
+
+    //-------------------------------------------------------
+    // DECLARE YOUR OWN VARIABLES AND FUNCTIONS HERE
+
+    int CpRace=0;  // Number of checkpoints
+    int TuRace=0;  // Number of Laps
+    int CpNext=0;  // Index of the next checkpoint
+    float sxAct=0; // Actual x position
+    float syAct=0; // Actual y position
+    float Ncpx=0;  // Next Checkpoint x position
+    float Ncpy=0;  // Next Checkpoint y position
+
+    // END OF VARIABLES/FUNCTIONS AREA
+    //-------------------------------------------------------
+
+    @Override
+    public void process(int delta)
+    {
+        //-------------------------------------------------------
+        // WRITE YOUR OWN CODE HERE
+
+        setPlayerName("70 - Banana");
+        selectShip(1);
+        setPlayerColor(255,255,255,255);
+
+        CpRace=getNbRaceCheckPoints();
+        TuRace=getNbMaxLaps();
+        CpNext=getNextCheckPointIndex();
+
+        moveToNextCheckPoint(0.5f);
+        incSpeed(1);
+
+
+        // END OF CODE AREA
+        //-------------------------------------------------------
+    }
+
+}
